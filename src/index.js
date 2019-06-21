11a82898ba0f12643a3c58d02e7cdee1e567aab6
Robbie Thandi <robbie.thandi@plus.net>
2019-06-21 16:04:32 +0100

    Added draw functionality.

--- src/index.js
+++ src/index.js
@@ -102,6 +102,8 @@ class Game extends React.Component {
         let status;
         if (winner) {
             status = 'Winner: ' + winner;
+        } else if (this.state.stepNumber >= 9){
+            status = 'Draw'
         } else {
             status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         }
@@ -142,7 +144,6 @@ function calculateWinner(squares) {
     }
     return null;
 }
-
 
 // ========================================
 
